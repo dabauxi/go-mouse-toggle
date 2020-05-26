ecb0dd70b2b06e69cc5c117db295b042599c688f
Paul Angerer <etimoz@noreply.github.com>
2020-05-26 17:55:55 +0200

    added fyne UI

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-mouse-toggle
 
 go 1.14
 
-require github.com/go-vgo/robotgo v0.0.0-20200520150636-18933afae07c
+require (
+	fyne.io/fyne v1.2.4
+	github.com/go-vgo/robotgo v0.0.0-20200520150636-18933afae07c
+)
--- main.go
+++ main.go
@@ -1,32 +1,117 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
 	"github.com/go-vgo/robotgo"
-	_"github.com/go-vgo/robotgo"
+	_ "github.com/go-vgo/robotgo"
+	"strconv"
 	"time"
 )
 
 func main() {
+	app := app.New()
+	duration := 0
+	w := app.NewWindow("Go mouse toggle")
+	isRunning := false
+	input := widget.NewEntry()
+	input.SetPlaceHolder("infiniteProgressBar")
+	errorLabel := widget.NewLabel("Wrong input, provide minutes.")
+	errorLabel.Hide()
+	progressBar := widget.NewLabel("Running 0/x minutes.")
+	infiniteProgressBar := widget.NewProgressBarInfinite()
+	progressBar.Hide()
+	infiniteProgressBar.Hide()
+	var quitChannel chan bool = nil
+	w.SetContent(widget.NewVBox(
+		progressBar,
+		infiniteProgressBar,
+		widget.NewLabel("Enter minutes:"),
+		errorLabel,
+		input,
+		widget.NewButton("Start", func() {
+			quitChannel = make(chan bool)
+			inputDuration := input.Text
+			i := 0
+			var err error = nil
+			if inputDuration != "" {
+				i, err = strconv.Atoi(input.Text)
+			}
+			if err != nil {
+				errorLabel.Show()
+				input.SetText("")
+			} else {
+				if isRunning {
+					return
+				}
+				errorLabel.Hide()
+				duration = i
+				isRunning = true
+				go func() {
+					if duration == 0 {
+						infiniteProgressBar.Show()
+					} else {
+						infiniteProgressBar.Hide()
+						progressBar.Show()
+						for i := 0; i < duration; i++ {
+							select {
+								case <- quitChannel:
+									break
+								default:
+									labelText := fmt.Sprintf("%d minutes left running.", i + 1)
+									progressBar.SetText(labelText)
+									time.Sleep(time.Second * 60)
+								}
+						}
+					}
+				}()
+				go runApp(duration, quitChannel)
+			}
+		}),
+		widget.NewButton("Stop", func() {
+			if isRunning {
+				quitChannel <- true
+				infiniteProgressBar.Hide()
+				progressBar.Hide()
+				close(quitChannel)
+				isRunning = false
+			}
+		}),
+		widget.NewButton("Quit", func() {
+			if isRunning {
+				quitChannel <- true
+				close(quitChannel)
+			}
+			app.Quit()
+		}),
+	))
+	w.ShowAndRun()
+}
+
+func runApp(duration int, quit chan bool) {
 	stepSize := 10
-	fmt.Println("Starting mouse toggle.")
-	duration := flag.Int("duration", 0, "Provide duration in minutes. (default 0 = infinite)")
-	flag.Parse()
 	_, maxY := robotgo.GetScreenSize()
-	if *duration == 0 {
-		fmt.Println("Duration: infinite")
+	if duration == 0 {
 		for {
-			toggle(maxY, stepSize)
+			select {
+			case <- quit:
+				return
+			default:
+				toggle(maxY, stepSize)
+			}
 		}
 	} else {
-		fmt.Printf("Duration: %d minutes\n", *duration)
-		seconds := *duration * 60
+		seconds := duration * 60
 		for i := 0; i < seconds; i = i+2 {
-			toggle(maxY, stepSize)
+			select {
+			case <-quit:
+				return
+			default:
+				toggle(maxY, stepSize)
+			}
 		}
 	}
-	fmt.Println("Mouse toggle session ended.")
 }
 
 func toggle(maxY, stepSize int) {
